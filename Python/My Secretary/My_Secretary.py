bdc9841b227a35a6360e1a82c08e3ee297cbdf58
Aiden Oh <aiden.hyunjae@gmail.com>
2021-04-12 22:44:40 -0400

    Update My_Secretary.py

--- Python/My Secretary/My_Secretary.py
+++ Python/My Secretary/My_Secretary.py
@@ -2,6 +2,11 @@ import re
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from datetime import datetime 
+
+def datetimenow():
+    now = datetime.now()
+    return (now)
 
 
 def create_soup(url):
@@ -12,11 +17,10 @@ def create_soup(url):
     return(soup)
 
 def today_weather():
-    
+    now = datetimenow() 
+
     url = "https://www.google.com/search?q=weather&oq=weather&aqs=chrome..69i57.968j0j15&sourceid=chrome&ie=UTF-8"
     soup = create_soup(url)
-
-    time = soup.find("div", attrs={"class":"wob_dts"}).get_text()
 
     cast = soup.find("img", attrs={"class":"wob_tci"})
     curr_temp = soup.find("span", attrs={"class":"wob_t TVtOme"}).get_text()
@@ -28,16 +32,38 @@ def today_weather():
     humid = soup.find("span", attrs={"id":"wob_hm"}).get_text()
     wind = soup.find("span", attrs={"id":"wob_ws"}).get_text()
 
-    print("[Today's Weather ({})]".format(time))
+    print("[Today's Weather ({})]".format(now.strftime('%Y-%m-%d')))
     print()
 
     print("Today is {}.".format(cast.get('alt').lower()))
     print("Current temperature is {}°. (Min {}°, Max {}°)".format(curr_temp, min_temp, max_temp))
     print()
     print("Precipitation : {}, Humidity : {}, Wind : {}.".format(prec, humid, wind))
+    print()
+
+
+def headline_news():
+    now = datetimenow()
+
+    url = "https://www.cnn.com"
+    soup = create_soup(url)
+    
+    print("[Today's headline news ({})]".format(now.strftime('%Y-%m-%d')))
+
+    headline = soup.find("span", attrs={"class":"cd__headline-icon-vid cnn-icon"})
+    for index, news in enumerate(headline):
+        title = news.find("a").get_text().strip() 
+        link = url + news.find("a")["href"]
+        print_news(index, title, link)
+    print()
+
+
+
+
+
 
 if __name__ == "__main__":
-    today_weather()
-    # headline_news()
+    # today_weather()
+    headline_news()
     # IT_news()
     # english_study()
