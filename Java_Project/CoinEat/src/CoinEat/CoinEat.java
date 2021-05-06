801151c47b2c69caaefddada22cb8314faf59a58
Aiden Oh <aiden.hyunjae@gmail.com>
2021-05-06 15:55:18 -0400

    Start HTML/CSS

--- Java_Project/CoinEat/src/CoinEat/CoinEat.java
+++ Java_Project/CoinEat/src/CoinEat/CoinEat.java
@@ -17,67 +17,67 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
-
-public class CoinEat extends JFrame{
+public class CoinEat extends JFrame {
 	private Image bufferImage;
 	private Graphics screenGraphic;
-	
+
 	private Clip clip;
-	
-	private Image backgroundImage = new ImageIcon("src/images/mainScreen.png").getImage();	
-	private Image player = new ImageIcon("src/images/player.png").getImage();	
-	private Image coin = new ImageIcon("src/images/coin.png").getImage();	
-	
+
+	private Image backgroundImage = new ImageIcon("src/images/mainScreen.png").getImage();
+	private Image player = new ImageIcon("src/images/player.png").getImage();
+	private Image coin = new ImageIcon("src/images/coin.png").getImage();
+
 	private int playerX, playerY;
 	private int playerWidth = player.getWidth(null);
 	private int playerHeight = player.getHeight(null);
-	
+
 	private int coinX, coinY;
 	private int coinWidth = coin.getWidth(null);
 	private int coinHeight = coin.getHeight(null);
-	
+
 	private int score;
-	
+
 	private boolean up, down, left, right;
-	
+
 	public CoinEat() {
 		setTitle("Coin Eat");
-//		setVisible(true);
+		// setVisible(true);
 		setSize(500, 500);
 		setLocationRelativeTo(null);
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		addKeyListener(new KeyAdapter() {
 			public void keyPressed(KeyEvent e) {
-				switch(e.getKeyCode()) {
-				case KeyEvent.VK_W:
-					up = true;
-					break;
-				case KeyEvent.VK_S:
-					down = true;
-					break;
-				case KeyEvent.VK_A:
-					left = true;
-					break;
-				case KeyEvent.VK_D:
-					right = true;
-					break;
+				switch (e.getKeyCode()) {
+					case KeyEvent.VK_W:
+						up = true;
+						break;
+					case KeyEvent.VK_S:
+						down = true;
+						break;
+					case KeyEvent.VK_A:
+						left = true;
+						break;
+					case KeyEvent.VK_D:
+						right = true;
+						break;
 				}
 			}
+
 			public void keyReleased(KeyEvent e) {
-				switch(e.getKeyCode()) {
-				case KeyEvent.VK_W:
-					up = false;
-					break;
-				case KeyEvent.VK_S:
-					down = false;
-					break;
-				case KeyEvent.VK_A:
-					left = false;
-					break;
-				case KeyEvent.VK_D:
-					right = false;
-					break;
+				switch (e.getKeyCode()) {
+					case KeyEvent.VK_W:
+						up = false;
+						break;
+					case KeyEvent.VK_S:
+						down = false;
+						break;
+					case KeyEvent.VK_A:
+						left = false;
+						break;
+					case KeyEvent.VK_D:
+						right = false;
+						break;
 				}
 			}
 		});
@@ -86,42 +86,48 @@ public class CoinEat extends JFrame{
 		while (true) {
 			try {
 				Thread.sleep(20);
-			} catch(InterruptedException e) {
+			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 			keyProcess();
 			crashCheck();
 		}
 	}
-	
+
 	public void Init() {
 		score = 0;
-		playerX = (500 - playerWidth)/2;
-		playerY = (500 - playerHeight)/2;
-		
-		coinX = (int)(Math.random()*(501-playerWidth));
-		coinY = (int)(Math.random()*(501-playerHeight-30))+30;
-		
+		playerX = (500 - playerWidth) / 2;
+		playerY = (500 - playerHeight) / 2;
+
+		coinX = (int) (Math.random() * (501 - playerWidth));
+		coinY = (int) (Math.random() * (501 - playerHeight - 30)) + 30;
+
 		playSound("src/audio/backgroundMusic.wav", true);
 	}
-	
+
 	public void keyProcess() {
-		if (up && playerY - 3 > 30) playerY -= 3;
-		if (down && playerY + playerHeight + 3 < 500) playerY +=3;
-		if (left && playerX - 3 > 30) playerX -= 3;
-		if (right && playerX + playerWidth + 3 < 500) playerX += 3;
+		if (up && playerY - 3 > 30)
+			playerY -= 3;
+		if (down && playerY + playerHeight + 3 < 500)
+			playerY += 3;
+		if (left && playerX - 3 > 30)
+			playerX -= 3;
+		if (right && playerX + playerWidth + 3 < 500)
+			playerX += 3;
 	}
-	
+
 	public void crashCheck() {
-		if (playerX+playerWidth > coinX && coinX+coinWidth > playerX && playerY+playerHeight > coinY && coinY+coinHeight > playerY) {
-			score+=100;
+		if (playerX + playerWidth > coinX && coinX + coinWidth > playerX && playerY + playerHeight > coinY
+				&& coinY + coinHeight > playerY) {
+			score += 100;
 			playSound("src/audio/CoinEat_src_audio_getCoin.wav", false);
-			coinX = (int)(Math.random()*(501-playerWidth));
-			coinY = (int)(Math.random()*(501-playerHeight-30))+30;
-			if (score == 2000) System.exit(0);
+			coinX = (int) (Math.random() * (501 - playerWidth));
+			coinY = (int) (Math.random() * (501 - playerHeight - 30)) + 30;
+			if (score == 2000)
+				System.exit(0);
 		}
 	}
-	
+
 	public void playSound(String pathName, boolean isLoop) {
 		try {
 			clip = AudioSystem.getClip();
@@ -129,37 +135,35 @@ public class CoinEat extends JFrame{
 			AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
 			clip.open(audioStream);
 			clip.start();
-			if(isLoop)
+			if (isLoop)
 				clip.loop(clip.LOOP_CONTINUOUSLY);
-		} catch(LineUnavailableException e) {
+		} catch (LineUnavailableException e) {
 			e.printStackTrace();
-		}
-		catch(UnsupportedAudioFileException e) {
+		} catch (UnsupportedAudioFileException e) {
 			e.printStackTrace();
-		}
-		catch(IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void paint(Graphics g) {
 		bufferImage = createImage(500, 500);
 		screenGraphic = bufferImage.getGraphics();
 		screenDraw(screenGraphic);
 		g.drawImage(bufferImage, 0, 0, null);
-		
+
 	}
-	
+
 	public void screenDraw(Graphics g) {
 		g.drawImage(backgroundImage, 0, 0, null);
 		g.drawImage(coin, coinX, coinY, null);
 		g.drawImage(player, playerX, playerY, null);
 		g.setColor(Color.WHITE);
 		g.setFont(new Font("Arial", Font.BOLD, 40));
-		g.drawString("SCORE : "+score, 30, 80);
+		g.drawString("SCORE : " + score, 30, 80);
 		this.repaint();
 	}
-	
+
 	public static void main(String[] args) {
 		new CoinEat();
 	}
